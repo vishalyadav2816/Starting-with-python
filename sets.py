25a4c833651fbe676de1748d98930269f7028127
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2022-11-27 22:15:37 +0530

    sets and some information about sets.

---
+++ sets.py
@@ -0,0 +1,20 @@
+#store mutliple values in single variable 
+#unordered
+
+
+
+
+# myset = ("car", "boat", "Bike")
+#print(myset)   
+
+#unchangeable
+
+
+#duplicates are not allowed 
+myset ={"cars","bike","bike"}
+print(myset)
+
+
+
+set = {1,2,3,4,5,6,7,8,9}
+print(len(set))
