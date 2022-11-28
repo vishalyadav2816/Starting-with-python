c83226ce1de2c6343c07ca16e003eeb95885058e
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2022-11-28 22:23:37 +0530

    How to swap the tuple

---
+++ reversetuple.py
@@ -0,0 +1,54 @@
+#tuples1 = ( 1 , 2,  3 , 4 , 5 )
+#print(tuples1[:: -1])
+
+#myname = ("vishal")
+#print(myname)
+
+#write  a program to unpack the following tuple into four variables and print each variable
+
+#(a, b, c, d) = tuple1
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+
+#swap above tuples
+#output - tuple1 = (30 , 40)
+         #tuple2 = (10 , 20)
+
+#tuple1 = (10 , 20)
+#tuple2 = (30, 40)
+
+#x = tuple1
+#tuple1=tuple2
+#tuple2=x
+
+
+#print(tuple1)
+#print(tuple2)
+
+#loops
+
+#mytuple = ("cars" , "boat" , "bike" )
+#for a in mytuple:
+#   print (a)
+
+#i = 0
+#while i < len(mytuple):
+#    print(mytuple[i])
+#    i += 1
+
+
+tuple1 = (1,[6,7],2,3,4,5,)
+#a =  tuple1[1][0]
+
+#print(a)
+
+a =  tuple1[8][0]
+
+
+
+
+
+
+
